54dbed3815d9fd97328be5b857ef315cda3805f1
dugga <dugga@MSI>
2023-10-21 15:50:25 -0700

    Finished all this

--- src/_03_cat/CatRunner.java
+++ src/_03_cat/CatRunner.java
@@ -1,5 +1,11 @@
 package _03_cat;
 
 public class CatRunner {
-
+public static void main(String[] args) {
+	new Cat(null);
+	Cat Cat2 = new Cat("cat3");
+	Cat2.meow();
+	Cat2.printName();
+	Cat2.kill();
+}
 }
---
+++ src/_04_harry_potter/HarryPotterRunner.java
@@ -0,0 +1,5 @@
+package _04_harry_potter;
+
+public class HarryPotterRunner {
+
+}
